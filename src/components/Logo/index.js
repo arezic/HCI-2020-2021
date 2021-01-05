8e5607bb2319938cca5eb495f91296cd86411ed2
Antonio Rezic <1antoniorezic@gmail.com>
2021-01-05 15:20:04 +0100

    Pokušaj dodavanja loga, treba još style za logo napravit, i zapravo prikazati sliku!

---
+++ src/components/Logo/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const Logo = () => (
+  <section className={styles.logo}>
+      <img src="logo.png"></img>
+  </section>
+)
+
+export default Logo
